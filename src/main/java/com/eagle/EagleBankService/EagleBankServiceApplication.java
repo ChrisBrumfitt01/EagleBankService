5bb844df2a07ef0e40e32373348ca4e938565435
chris <chrisbrumfitt@hotmail.co.uk>
2025-07-23 13:47:20 +0100

    Initial commit

---
+++ src/main/java/com/eagle/EagleBankService/EagleBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.EagleBankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eagle/EagleBankService/EagleBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.EagleBankService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
